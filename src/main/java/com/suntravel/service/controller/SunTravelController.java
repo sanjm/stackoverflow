633930f865028800a2cdcf12e824c588c0dfceda
ShaiM <your_email@example.com>
2016-03-10 22:40:08 -0800

    http://stackoverflow.com/questions/35932517/

---
+++ src/main/java/com/suntravel/service/controller/SunTravelController.java
@@ -0,0 +1,44 @@
+package com.suntravel.service.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/suntravel")
+public class SunTravelController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SunTravelController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//
+//		String formattedDate = dateFormat.format(date);
+//
+//		model.addAttribute("serverTime", formattedDate );
+//
+//		return "home";
+//	}
+
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET, produces="application/json")
+	public String getHotelList( @PathVariable String name )
+	{
+		System.out.println("name:"+name);
+		return name;
+
+	}
+
+}
